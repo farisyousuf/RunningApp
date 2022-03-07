48a32e8b0b79c2b66661f51d0af9a0bf206ad020
farisyousuf <farisyousuf@gmail.com>
2022-03-07 12:15:36 +0400

    Run List implemented

--- app/src/main/java/com/faris/runningapp/ui/view/RunFragment.kt
+++ app/src/main/java/com/faris/runningapp/ui/view/RunFragment.kt
@@ -13,6 +13,8 @@ import androidx.core.content.ContextCompat
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.viewModels
 import androidx.navigation.fragment.findNavController
+import androidx.recyclerview.widget.LinearLayoutManager
+import com.faris.runningapp.adapters.RunAdapter
 import com.faris.runningapp.databinding.FragmentRunBinding
 import com.faris.runningapp.ui.viewmodels.MainViewModel
 import dagger.hilt.android.AndroidEntryPoint
@@ -28,7 +30,7 @@ class RunFragment : Fragment() {
     private var fineLocationGranted = false
     private var coarseLocationGranted = false
     private lateinit var locationPermissionRequest: ActivityResultLauncher<Array<String>>
-
+    private lateinit var runAdapter: RunAdapter
     override fun onCreateView(
         inflater: LayoutInflater,
         container: ViewGroup?,
@@ -42,8 +44,20 @@ class RunFragment : Fragment() {
         super.onViewCreated(view, savedInstanceState)
         createLocationPermissionsRequest()
         checkOrUpdatePermissions()
+        setupRecyclerView()
+        viewModel.runsSortedByDate.observe(viewLifecycleOwner) {
+            runAdapter.submitList(it)
+        }
         binding.fab.setOnClickListener {
             findNavController().navigate(RunFragmentDirections.actionRunFragmentToTrackingFragment())
+        }
+    }
+
+    private fun setupRecyclerView() {
+        binding.rvRuns.apply {
+            runAdapter = RunAdapter()
+            adapter = runAdapter
+            layoutManager = LinearLayoutManager(requireContext())
         }
     }
 
